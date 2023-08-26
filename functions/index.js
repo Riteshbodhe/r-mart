f5d1b90b8ac4c35c5cff2165e0f526476a5e0e99
Riteshbodhe <riteshbodhe12@gmail.com>
2023-08-26 15:51:01 +0530

    deployed suceessfully

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,40 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")
-('sk_test_51NeC5eSAfG2bMhAEAqfYK4rLKmhb3PafdUmEGKafeew7l2lkNesHmQPoMay5R1UlWzRIKQArCuN0Tk7CC0UTznT500hFkSLvhw')
+const stripe = require("stripe")(
+    "sk_test_51NeC5eSAfG2bMhAEAqfYK4rLKmhb3PafdUmEGKafeew7l2lkNesHmQPo" +
+    "May5R1UlWzRIKQArCuN0Tk7CC0UTznT500hFkSLvhw",
+);
 
 // API
 
 
 // App config
-
- const app = express();
+const app = express();
 // Middlewares
-app.use(cors({ origin: true}));
+app.use(cors({origin: true}));
 app.use(express.json());
 
 
-//API routes 
-app.get('/', (request, response) => response.status(200).send("hello world"))
+// API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
 
-app.post("/payments/create", async (request, response) =>{
-    const total = request.query.total;
-    console.log("Payment Request recieved for this Amount >>", total )
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  console.log("Payment Request recieved for this Amount >>", total );
 
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: total, //subuints of the currency
-        currency: "inr",
-    });
-    //ok - created
-    response.status(201).send({
-        clientSecret: paymentIntent.client_secret,
-    })
-})
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subuints of the currency
+    currency: "inr",
+  });
+    // ok - created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
-//LIsten command
-exports.api = functions.https.onRequest(app)
+// LIsten command
+exports.api = functions.https.onRequest(app);
 
+// Example endpoint
 // http://127.0.0.1:5001/r--mart/us-central1/api
--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,10 @@
 
  const instance = axios.create({
 //THE API URL(CLOUD FUNCTION)
-    baseURL: 'http://127.0.0.1:5001/r--mart/us-central1/api' 
+    baseURL: "https://us-central1-r--mart.cloudfunctions.net/api"
+    //'http://127.0.0.1:5001/r--mart/us-central1/api' 
  });
 
  export default instance;
+
+ 
